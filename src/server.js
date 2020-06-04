00f898e3f12321a6f8f44c832ea292b505d410ca
carlosm3 <carlosedsb0110@gmail.com>
2020-06-04 12:11:40 -0300

    aula 4 finalizada

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+//configurar rotas da aplicacao
+//página inicial
+//req: Request
+//res: Response
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título" })
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+//ligar o servidor 
+server.listen(3000)
+
